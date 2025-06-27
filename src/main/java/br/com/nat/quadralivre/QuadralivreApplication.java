942b444fd68b70b82921894a06d3cfc11abaf677
nataliafrancisca <nathifrancisca@gmail.com>
2025-06-26 23:44:19 -0300

    feat: init repositorio

---
+++ src/main/java/br/com/nat/quadralivre/QuadralivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.nat.quadralivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadralivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadralivreApplication.class, args);
+	}
+
+}
