61cbbffb287afffeaeb9753c2a3738c66ddd7bca
nataliafrancisca <nathifrancisca@gmail.com>
2025-06-29 22:57:30 -0300

    feat: criando um ENUM para DiaSemana, onde ela é usada no cadastro de funcionamento da quadra.

---
+++ src/main/java/br/com/nat/quadralivre/domain/quadra/funcionamento/DiaSemana.java
@@ -0,0 +1,13 @@
+package br.com.nat.quadralivre.domain.quadra.funcionamento;
+
+public enum DiaSemana {
+    DOMINGO ("SUNDAY"),
+    SEGUNDA ("MONDAY"),
+    TERCA ("TUESDAY"),
+    QUARTA ("WEDNESDAY"),
+    QUINTA ("THURSDAY"),
+    SEXTA ("FRIDAY"),
+    SABADO ("SATURDAY");
+
+    DiaSemana(String diaDaSemanaEmIngles){}
+}
