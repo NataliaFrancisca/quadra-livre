4f97f5b5bf857bf7d2c96c4da14eaed15b6f2f4b
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-01 00:01:55 -0300

    refactor: troquei a validação de @NotBlank para @NotNull

--- src/main/java/br/com/nat/quadralivre/domain/quadra/QuadraAtualizacao.java
+++ src/main/java/br/com/nat/quadralivre/domain/quadra/QuadraAtualizacao.java
@@ -2,9 +2,10 @@ package br.com.nat.quadralivre.domain.quadra;
 
 import br.com.nat.quadralivre.domain.quadra.endereco.EnderecoRegistro;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record QuadraAtualizacao (
-        @NotBlank
+        @NotNull
         Long id,
         String nome,
         EnderecoRegistro endereco
