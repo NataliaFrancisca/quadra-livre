27394a75780d26f797c3f4086e47431fdb716ce4
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-01 21:54:02 -0300

    feat: adicionando novas queries

--- src/main/java/br/com/nat/quadralivre/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/nat/quadralivre/domain/usuario/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.nat.quadralivre.domain.usuario;
 
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,4 +13,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     @Query("SELECT u FROM Usuario u WHERE u.login LIKE :login")
     Optional<Usuario> buscarUsuarioPorCampoLogin(String login);
+
+    boolean existsByLogin(String login);
+    boolean existsByCpf(String cpf);
 }
