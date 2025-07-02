cd561e4c473fa21ae29ea68bf7839b101d4b8f98
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-01 21:53:05 -0300

    feat: adicionando mensagens nas anotações de validação dos campos de registro.

--- src/main/java/br/com/nat/quadralivre/domain/usuario/UsuarioRegistro.java
+++ src/main/java/br/com/nat/quadralivre/domain/usuario/UsuarioRegistro.java
@@ -1,20 +1,21 @@
 package br.com.nat.quadralivre.domain.usuario;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record UsuarioRegistro(
         @NotBlank
+        @Email(message = "Digite um e-mail válido. Exemplo: email@email.com")
         String login,
         @NotBlank
+        @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres." )
         String senha,
-        @NotBlank
+        @NotBlank(message = "O campo nome é obrigatório.")
         String nome,
-        @NotBlank
+        @NotBlank(message = "O campo telefone é obrigatório.")
         String telefone,
         @NotBlank
-        @CPF
+        @CPF(message = "Digite um número de CPF válido.")
         String cpf,
         @NotNull(message = "Digite um valor válido. Ex.: 'GESTOR' ou 'USUARIO'.")
         Role role
