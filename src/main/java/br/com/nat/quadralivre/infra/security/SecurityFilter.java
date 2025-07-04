d92b38ccf90c7f58f49f8fc50c783d889033bf39
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-03 22:23:57 -0300

    feat: modificando o retorno quando o token é inválido.

--- src/main/java/br/com/nat/quadralivre/infra/security/SecurityFilter.java
+++ src/main/java/br/com/nat/quadralivre/infra/security/SecurityFilter.java
@@ -1,6 +1,7 @@
 package br.com.nat.quadralivre.infra.security;
 
 import br.com.nat.quadralivre.domain.usuario.UsuarioRepository;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,13 +27,21 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = this.recuperarToken(request);
 
-        if (tokenJWT != null){
+        try{
             var subject  = this.tokenService.getSubject(tokenJWT);
             var usuario = this.usuarioRepository.findByLogin(subject);
-
             var autenticacao = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
 
             SecurityContextHolder.getContext().setAuthentication(autenticacao);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("""
+                    {
+                      "erro": "Token JWT inválido ou expirado. Por favor, faça login novamente."
+                    }
+                    """);
+            return;
         }
 
         filterChain.doFilter(request, response);
