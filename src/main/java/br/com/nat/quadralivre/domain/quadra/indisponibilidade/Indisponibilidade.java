e87b5532d31855c048f7466cc730a3901f9c4614
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-06 21:03:08 -0300

    feat: criando arquivos necessários para o funcionamento da entidade Indisponibilidade

---
+++ src/main/java/br/com/nat/quadralivre/domain/quadra/indisponibilidade/Indisponibilidade.java
@@ -0,0 +1,28 @@
+package br.com.nat.quadralivre.domain.quadra.indisponibilidade;
+
+import br.com.nat.quadralivre.domain.quadra.Quadra;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity(name = "Indisponibilidade")
+@Table(name = "indisponibilidades")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Indisponibilidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @ManyToOne
+    private Quadra quadra;
+}
