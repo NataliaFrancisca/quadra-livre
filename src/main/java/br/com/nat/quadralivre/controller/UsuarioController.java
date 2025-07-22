15273965d5170a207ee57a21db6d0e4b432d89cd
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-21 23:36:19 -0300

    refactor: reformulando todos os métodos para uma melhor organização e padronização dos métodos;

--- src/main/java/br/com/nat/quadralivre/controller/UsuarioController.java
+++ src/main/java/br/com/nat/quadralivre/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.nat.quadralivre.controller;
 
 import br.com.nat.quadralivre.domain.usuario.*;
+import br.com.nat.quadralivre.domain.usuario.autenticacao.UsuarioAutenticacao;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
@@ -18,42 +17,43 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    private URI generateURI(Long id, UriComponentsBuilder uriBuilder){
-        return uriBuilder
-                .path("/usuarios/{id}")
-                .buildAndExpand(id)
-                .toUri();
+    @PostMapping("/login")
+    public ResponseEntity efetuarLogin(@RequestBody @Valid UsuarioAutenticacao usuarioLogin){
+        var dados = this.usuarioService.login(usuarioLogin);
+        return ResponseEntity.ok(dados);
     }
 
-    @PostMapping
+    @PostMapping("/registrar")
     @Transactional
-    public ResponseEntity registrarUsuario(@RequestBody @Valid UsuarioRegistro usuarioRegistro, UriComponentsBuilder uri){
+    public ResponseEntity registrarUsuario(@RequestBody @Valid UsuarioRegistro usuarioRegistro, UriComponentsBuilder uriBuilder){
         var usuario = this.usuarioService.registrar(usuarioRegistro);
-        return ResponseEntity.created(this.generateURI(usuario.id(), uri)).body(usuario);
+
+        var uri = uriBuilder
+                .path("/usuarios/{id}")
+                .buildAndExpand(usuario.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(usuario);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity buscarUsuario(@PathVariable Long id, Authentication authentication){
-        var usuarioLogado = (Usuario) authentication.getPrincipal();
-        var usuario = this.usuarioService.buscar(id, usuarioLogado);
-
-        return ResponseEntity.ok(usuario);
+    public ResponseEntity buscarUsuario(@PathVariable Long id, Authentication auth){
+        var usuario = (Usuario) auth.getPrincipal();
+        return ResponseEntity.ok(this.usuarioService.buscar(id, usuario));
     }
 
     @PutMapping
     @Transactional
-    public ResponseEntity atualizarUsuario(@RequestBody @Valid UsuarioAtualizacao usuarioAtualizacao, Authentication authentication){
-        var usuarioLogado = (Usuario) authentication.getPrincipal();
-        var usuario = this.usuarioService.atualizar(usuarioLogado.getLogin(), usuarioAtualizacao);
-
-        return ResponseEntity.ok(usuario);
+    public ResponseEntity atualizarUsuario(@RequestBody @Valid UsuarioAtualizacao usuarioAtualizacao, Authentication auth){
+        var usuario = (Usuario) auth.getPrincipal();
+        return ResponseEntity.ok(this.usuarioService.atualizar(usuarioAtualizacao, usuario));
     }
 
     @DeleteMapping
     @Transactional
-    public ResponseEntity deletarUsuario(Authentication authentication){
-        var usuarioLogado = (Usuario) authentication.getPrincipal();
-        this.usuarioService.deletar(usuarioLogado.getId());
+    public ResponseEntity deletarUsuario(Authentication auth){
+        var usuario = (Usuario) auth.getPrincipal();
+        this.usuarioService.deletar(usuario);
         return ResponseEntity.noContent().build();
     }
 }
