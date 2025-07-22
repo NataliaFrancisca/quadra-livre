6bcd61511e84ea302173bd3c2889a57823f8c547
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-22 00:07:14 -0300

    style: removendo linha em branco

--- src/main/java/br/com/nat/quadralivre/domain/quadra/QuadraRepository.java
+++ src/main/java/br/com/nat/quadralivre/domain/quadra/QuadraRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuadraRepository extends JpaRepository<Quadra, Long> {
     boolean existsByNomeAndEndereco(String nome, Endereco endereco);
-
     boolean existsByGestorEmail(String email);
 }
