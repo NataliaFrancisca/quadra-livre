6b74281c9f2a9754a6768eab5d81e37b816644cd
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-21 21:12:54 -0300

    feat: adicionando as novas propriedades (minutosReserva e minutosIntervalo), dessa forma o gestor tem flexibilidade para definir o tempo para cada reserva.

--- src/main/java/br/com/nat/quadralivre/domain/quadra/QuadraDadosDetalhados.java
+++ src/main/java/br/com/nat/quadralivre/domain/quadra/QuadraDadosDetalhados.java
@@ -7,9 +7,11 @@ public record QuadraDadosDetalhados(
         Long id,
         String nome,
         Endereco endereco,
+        Integer minutosReserva,
+        Integer minutosIntervalo,
         UsuarioDadosAberto gestor
 ) {
     public QuadraDadosDetalhados(Quadra quadra){
-        this(quadra.getId(), quadra.getNome(), quadra.getEndereco(), new UsuarioDadosAberto(quadra.getGestor()));
+        this(quadra.getId(), quadra.getNome(), quadra.getEndereco(), quadra.getMinutosReserva(), quadra.getMinutosIntervalo(), new UsuarioDadosAberto(quadra.getGestor()));
     }
 }
