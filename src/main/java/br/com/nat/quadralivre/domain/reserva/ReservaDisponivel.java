3621bd6b2a7080f23d58cd86d17f5b81971288a5
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-25 21:40:27 -0300

    feat: alterando o arquivo de record para uma classe porque dessa forma consigo setar o id diretamente no arquivo.

--- src/main/java/br/com/nat/quadralivre/domain/reserva/ReservaDisponivel.java
+++ src/main/java/br/com/nat/quadralivre/domain/reserva/ReservaDisponivel.java
@@ -1,12 +1,33 @@
 package br.com.nat.quadralivre.domain.reserva;
 
+import br.com.nat.quadralivre.domain.quadra.funcionamento.HorarioFuncionamento;
+import lombok.Getter;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.UUID;
 
-public record ReservaDisponivel(
-        String id,
-        LocalTime abertura,
-        LocalTime fechamento,
-        LocalDateTime data
-) {
+@Getter
+public class ReservaDisponivel {
+    private String id;
+    private LocalTime inicio;
+    private LocalTime encerramento;
+    private LocalDateTime data;
+
+    public ReservaDisponivel(HorarioFuncionamento horarioFuncionamento, LocalDateTime horarioReserva, LocalDate dataSolicitada, int minutosReserva){
+        this.id = this.gerarHashComoString(horarioFuncionamento, horarioReserva);
+        this.inicio = horarioReserva.toLocalTime();
+        this.encerramento = horarioReserva.toLocalTime().plusMinutes(minutosReserva);
+        this.data = dataSolicitada.atTime(horarioReserva.toLocalTime());
+    }
+
+    private String gerarHashComoString(HorarioFuncionamento funcionamentoDados, LocalDateTime horarioReserva){
+        return UUID
+                .nameUUIDFromBytes((
+                        horarioReserva.toString() +
+                                funcionamentoDados.getQuadra().getId() +
+                                funcionamentoDados.getDia()).getBytes()
+                ).toString();
+    }
 }
