8b86b21637306b8292f2918cc0f4e06455565931
nataliafrancisca <nathifrancisca@gmail.com>
2025-07-25 23:19:01 -0300

    feat: criando arquivo de configuração para o Swagger

---
+++ src/main/java/br/com/nat/quadralivre/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,28 @@
+package br.com.nat.quadralivre.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer").bearerFormat("JWT")
+                                )
+                )
+
+                .info(new Info()
+                        .title("Quadra Livre API")
+                        .description("API Rest da aplicação Quadra Livre, contendo as funcionalidades de CRUD para Usuario, Quadra e Reserva.")
+                );
+    }
+}
